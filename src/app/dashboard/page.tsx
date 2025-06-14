19b227b80418a770028362fb3e43948daf2f2146
Guilherme Araujo <guiluis.aa@gmail.com>
2025-06-14 08:39:29 -0300

    feat: styled-components setup and theme

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@ui/Button';
+
 import { CharactersSection } from './_components/CharactersSection';
 
 export default async function Page() {
   return (
     <main>
       <h1>Dashboard</h1>
+      <Button>Click me</Button>
 
       <CharactersSection />
     </main>
---
+++ src/lib/theme/tokens/font.ts
@@ -0,0 +1,13 @@
+import color from './color';
+
+const font = {
+  family: "'Nunito Sans', sans-serif",
+  weight: { regular: 400, bold: 500 },
+  color: {
+    primary: color.typography.primary,
+    secondary: color.typography.secondary
+  }
+};
+
+export type Font = typeof font;
+export default font;
